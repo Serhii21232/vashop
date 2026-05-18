a7acf12cc3e28ab006694cd3f675330b87c4149f
Serhii21232 <ldem100ffgg@gmail.com>
2026-05-18 10:30:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,14 +15,17 @@ def start_message(message):
 
     bot.send_message(
         message.chat.id,
-        f"Привет, {username}!\n\n"
-        f"Вот официальная ссылка на канал:\n{CHANNEL_LINK}\n\n"
+        f"👋 Привет, {username}! Добро пожаловать в 💨 VAPE PLUG📉!\n\n"
+        f"Надоело переплачивать за аренду и наценки обычных магазинов?\n"
+        f"Мы закупаем товар напрямую большими партиями и делимся выгодой с тобой.\n\n"
+        f"🔥 Для получения ссылки в основной канал пиши /start 🤠\n\n"
+        f"📢 Официальная ссылка на канал:\n{CHANNEL_LINK}\n\n"
         f"🤖 Бот сделан warilidi\n"
-        f"По поводу заказа писать на FunPay:\n{FUNPAY_LINK}"
+        f"💬 По поводу заказа писать на FunPay:\n{FUNPAY_LINK}"
     )
 
 
-@bot.message_handler(func=lambda message: message.text and message.text.lower() in [
+@bot.message_handler(func=lambda m: m.text and m.text.lower() in [
     "привет",
     "здравствуйте",
     "hello",
@@ -33,12 +36,11 @@ def hello_message(message):
 
     bot.send_message(
         message.chat.id,
-        f"Привет, {username}!\n\n"
-        f"Вот официальная ссылка на канал:\n{CHANNEL_LINK}\n\n"
-        f"🤖 Бот сделан warilidi\n"
-        f"По поводу заказа писать на FunPay:\n{FUNPAY_LINK}"
+        f"👋 Привет, {username}!\n\n"
+        f"📢 Официальная ссылка на канал:\n{CHANNEL_LINK}"
     )
 
 
-print("Бот запущен...")
-bot.infinity_polling()
+print("BOT STARTED")
+
+bot.infinity_polling(skip_pending=True)
